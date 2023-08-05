a0b8b4bf53575c146f540a7c0d135da2e6d7aa6a
Sergey Divaev <divaev.sergey@gmail.com>
2023-08-05 20:35:14 +0300

    Начало работы над BurgerConstructor

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,13 +2,15 @@ import styles from "./app.module.css";
 import { data } from "../../utils/data";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients"
+import BurgerConstructor from "../burger-constructor/burger-constructor"
 
 function App() {
   return (
     <div className={styles.app}>
       <AppHeader />
-      <main>
+      <main className={styles.main}>
         <BurgerIngredients data={data} />
+        <BurgerConstructor data={data} />
       </main>
     </div>
   );
