51e6fbd99c6da2f1d4f96286d0ba88b324e7c107
Sergey Divaev <divaev.sergey@gmail.com>
2023-08-05 19:57:41 +0300

    Закончен Header, добавлен BurgerIngredients

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,33 +1,31 @@
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import appHeader from './app-header.module.css';
+import styles from './app-header.module.css';
 
 const AppHeader = () => {
     return (
-        <>
-            <header className={appHeader.header}>
-                <nav>
-                    <ul className={appHeader.navList}>
-                        <li className={appHeader.navItem}>
-                            <BurgerIcon type="primary"/>
-                            <h3 className="text text_type_main-default">Конструктор</h3>
-                        </li>
-                        <li className={appHeader.navItem}>
-                            <ListIcon type="secondary"/>
-                            <h3 className="text text_type_main-default text_color_inactive">Лента заказов</h3>
-                        </li>
-                        <li className={appHeader.navLogp}>
-                            <Logo/>
-                        </li>
-                        <li className={appHeader.navItem}>
-                            <ProfileIcon type="secondary"/>
-                            <h3 className="text text_type_main-default text_color_inactive">Личный кабинет</h3>
-                        </li>
-                    </ul>                
-                </nav>
-            </header>
-        </>        
+        <header className={styles.header}>
+            <nav>
+                <ul className={styles.nav}>
+                    <li className={styles.nav_item_constructor}>
+                        <BurgerIcon type="primary" />
+                        <p className="text text_type_main-default">Конструктор</p>
+                    </li>
+                    <li className={styles.nav_item_order_feed}>
+                        <ListIcon type="secondary" />
+                        <p className="text text_type_main-default text_color_inactive">Лента заказов</p>
+                    </li>
+                    <li className={styles.logo}>
+                        <Logo />
+                    </li>
+                    <li className={styles.nav_item_lk}>
+                        <ProfileIcon type="secondary" />
+                        <p className="text text_type_main-default text_color_inactive">Личный кабинет</p>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
-    }
-    
+}
+
 
 export default AppHeader;
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,15 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
-import AppHeader from "../app-header/app-header"
+import AppHeader from "../app-header/app-header";
+import BurgerIngredients from "../burger-ingredients/burger-ingredients"
 
 function App() {
   return (
     <div className={styles.app}>
-      <AppHeader/>       
+      <AppHeader />
+      <main>
+        <BurgerIngredients data={data} />
+      </main>
     </div>
   );
 }
