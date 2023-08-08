503894e39e3dff8c221002014d1a2dbb4a9d34ad
SD <divaev.sergey@gmail.com>
2023-08-08 21:24:09 +0300

    В BurgerConstructor добавлена мемоизация данных

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,22 +1,23 @@
+import React from 'react';
 import { ConstructorElement, DragIcon, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './burger-constructor.module.css';
 import { ingredientPropType } from '../../utils/prop-types';
 
 const BurgerConstructor = ({ data }) => {
 
-    const ingredientList = data.filter(item => item.type == "sauce" || item.type == "main");
-    const bun = data.filter(item => item.type == "bun");
-    const price = ingredientList.reduce((sum, item) => { return sum + item.price }, 0) + bun.reduce((sum, item) => { return sum + item.price }, 0);
+    const ingredientList = React.useMemo(() => data.filter(item => item.type == "sauce" || item.type == "main"), [data]);
+    const bunList = React.useMemo(() => data.filter(item => item.type == "bun"), [data]);
+    const price = React.useMemo(() => ingredientList.reduce((sum, item) => { return sum + item.price }, 0) + bunList.reduce((sum, item) => { return sum + item.price }, 0), [data]);
 
     return (
         <section className={styles.section}>
             {
-                bun.length != 0 && (
+                bunList.length > 0 && (
                     <ConstructorElement
                         type="top"
-                        text={bun[0].name + ' ' + '(верх)'}
-                        price={bun[0].price}
-                        thumbnail={bun[0].image}
+                        text={bunList[0].name + ' ' + '(верх)'}
+                        price={bunList[0].price}
+                        thumbnail={bunList[0].image}
                         isLocked='true'
                         extraClass='ml-8 mr-2'
                     />
@@ -37,12 +38,12 @@ const BurgerConstructor = ({ data }) => {
                 }
             </ul>
             {
-                bun.length != 0 && (
+                bunList.length > 0 && (
                     <ConstructorElement
                         type="bottom"
-                        text={bun[1].name + ' ' + '(низ)'}
-                        price={bun[1].price}
-                        thumbnail={bun[1].image}
+                        text={bunList[1].name + ' ' + '(низ)'}
+                        price={bunList[1].price}
+                        thumbnail={bunList[1].image}
                         isLocked='true'
                         extraClass='ml-8 mr-2'
                     />
